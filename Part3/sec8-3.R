1d385766e44a4c8ef9c80d000df32150aca019a9
jcy1302 <za474ch@gmail.com>
2019-06-03 17:47:48 +0900

    Part 3, stringr package

---
+++ Part3/sec8-3.R
@@ -0,0 +1,44 @@
+install.packages('reshape2')
+library("reshape2")
+
+fruits
+
+melt(Fruits, id='Year')
+melt(Fruits, id=c('Year', 'Fruit'))
+
+install.packages('stringr')
+library(stringr)
+
+fruits <- c('apple', 'Apple', 'banana', 'pineapple')
+str_detect(fruits, 'A')
+str_detect(fruits, 'a')
+str_detect(fruits, '^a') # ^:단어의 처음, $:단어의 끝
+str_detect(fruits, 'e$')
+str_detect(fruits, '^[aA]')
+str_detect(fruits, '[aA]')
+str_detect(fruits, regex('a', ignore_case = T))
+fruits
+str_count(fruits, 'a')           
+str_c("apple","banana")
+str_c("fruits:", fruits)
+str_c(fruits, " name is ", fruits)
+str_c(fruits, collapse="")
+str_c(fruits, collapse=",")
+str_c(fruits, collapse=", ")
+str_dup(fruits,3)
+str_length('과일') # 한글자 1로 인식하여 편리함
+str_locate('apple', 'a')
+str_locate(fruits, 'a')
+str_locate('apple', 'app')
+str_replace('apple','p','*')
+str_replace('apple','p','++')
+str_replace_all('apple','p','*')
+fruits <- str_c('apple','/','orange','/','banana')
+fruits
+str_split(fruits, '/') ## list형으로 출력
+str_sub(fruits, start=1, end=3)
+str_sub(fruits, start=6, end=9)
+str_sub(fruits, start=7)
+str_sub(fruits, start=-5)
+str_trim('\t apple banana berry \n') # \t or \n도 지워짐
+
